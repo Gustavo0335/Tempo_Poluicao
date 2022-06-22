d33f6da79ae07085035c9c5b85b36d9ed9cc41fc
Gustavo0335 <guga2605@outlook.com>
2022-06-22 16:40:36 -0300

    Exercicio

---
+++ tempo/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import{ HttpClientModule} from '@angular/common/http';
+
+
+import { AppComponent } from './app.component';
+import { TodayComponent } from './hoje/hoje.component';
+import { FutureComponent } from './futuro/futuro.component';
+import { PollutionComponent } from './poluicao/poluicao.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodayComponent,
+    FutureComponent,
+    PollutionComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
